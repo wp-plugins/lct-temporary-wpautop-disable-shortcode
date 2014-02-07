2153636fdb2d74c72f11a2de820ab5c4b879b94e
ircary <ircary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-07 18:45:48 +0000

    finished v1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/lct-temporary-wpautop-disable-shortcode/branches/lct-temporary-wpautop-disable-shortcode-v1.1.1@853380 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lct-temporary-wpautop-disable.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: LCT Temporary wpautop Disable Shortcode
+Plugin URI: http://lookclassy.com/wordpress-plugins/temporary-wpautop-disable/
+Description: Use a simple shortcode to bypass the sometimes damaging effect of the wpautop function.
+Version: 1.1.1
+Text Domain: lct-temporary-wpautop-disable
+Author: Look Classy Technologies
+Author URI: http://lookclassy.com/
+License: GPLv3 (http://opensource.org/licenses/GPL-3.0)
+Copyright 2013 Look Classy Technologies  (email : info@lookclassy.com)
+*/
+
+/*
+Copyright (C) 2013 Look Classy Technologies
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+remove_filter('the_content', 'wpautop');
+remove_filter('the_content', 'wptexturize');
+add_filter('the_content', 'wpautop_Disable', 99);
+function wpautop_Disable($content) {
+	$new_content = '';
+	$pattern_full = '{(\[raw\].*?\[/raw\])}is';
+	$pattern_contents = '{\[raw\](.*?)\[/raw\]}is';
+	$pieces = preg_split($pattern_full, $content, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+	foreach($pieces as $piece) {
+		if(preg_match($pattern_contents, $piece, $matches)) {
+			$new_content .= $matches[1];
+		} else {
+			$new_content .= wptexturize(wpautop($piece));
+		}
+	}
+
+	return $new_content;
+}
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== LCT Temporary wpautop Disable Shortcode ===
+Contributors: ircary
+Donate link: http://lookclassy.com/
+Tags: shortcode, disable wpautop
+Requires at least: 3.0
+Tested up to: 3.8.1
+Stable tag: 1.1.1
+License: GPLv3 or later
+License URI: http://opensource.org/licenses/GPL-3.0
+
+Use a simple shortcode to bypass the sometimes damaging effect of the wpautop function.
+
+
+== Description ==
+Use a simple shortcode to bypass the sometimes damaging effect of the wpautop function.
+
+== Installation ==
+1. Upload the zip file contents to your Wordpress plugins directory.
+2. Go to the Plugins page in your WordPress Administration area and click 'Activate' for LCT Temporary wpautop Disable Shortcode.
+3. Use this shortcode to disable wpautop for the content within [raw]Code you don't want filter by wpautop[/raw]
+
+== Screenshots ==
+none
+
+== Frequently Asked Questions ==
+none
+
+
+== Upgrade Notice ==
+none
+
+
+== Changelog ==
+= 1.1.1 =
+	- Tested for WP 3.8.1 Compapibility
+
+= 1.0 =
+	- First Release
